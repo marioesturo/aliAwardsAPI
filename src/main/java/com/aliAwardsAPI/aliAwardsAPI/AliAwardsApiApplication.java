dd0605e3c0a09b69eea0d95f746adc4c059ed827
marioesturo <marioesturo@gmail.com>
2024-10-17 13:04:05 +0200

    Initial commit

---
+++ src/main/java/com/aliAwardsAPI/aliAwardsAPI/AliAwardsApiApplication.java
@@ -0,0 +1,13 @@
+package com.aliAwardsAPI.aliAwardsAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliAwardsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AliAwardsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aliAwardsAPI/aliAwardsAPI/AliAwardsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aliAwardsAPI.aliAwardsAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AliAwardsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
